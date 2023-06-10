bd7e8e981f1d6231150392c6a511b7cfdb439f75
Almaz <bakievalmaz0@gmail.com>
2023-06-10 04:02:46 +0300

    homework5 completed

--- Homework_C#5/Task34/Program.cs
+++ Homework_C#5/Task34/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = GetAndGenerateElementsInArray();
+PrintArray(array);
+SearchForAnEvenNumbers(array);
+
+int[] GetAndGenerateElementsInArray()
+{
+    Random random = new Random();
+    int[] result = new int[5];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(100, 1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    foreach (int el in array)
+    {
+        Console.Write($"{el} ");
+    }
+    Console.Write("]");
+}
+
+void SearchForAnEvenNumbers(int[] array)
+{
+    int counter = 0;
+    foreach (int el in array)
+    {
+        if ((el & 1) == 0)
+        {
+            counter++;
+        }
+    }
+    Console.WriteLine(" -> " + counter);
+}
--- Homework_C#5/Task38/Program.cs
+++ Homework_C#5/Task38/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[] array = GetAndGenerateElementsInArray();
+double min = array[0];
+double max = array[0];
+PrintArray(array);
+SearchMinMax(array);
+
+double[] GetAndGenerateElementsInArray()
+{
+    Random random = new Random();
+    double[] result = new double[5];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = random.NextDouble() * 10 - 5;
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{string.Format("{0:F2}", array[i])} ");
+    }
+    Console.Write("]");
+}
+
+void SearchMinMax(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i]) min = array[i];
+        if (max < array[i]) max = array[i];
+    }
+    Console.WriteLine($" => {string.Format("{0:F2}", max)} - {string.Format("{0:F2}", min)} = {string.Format("{0:F2}", max - min)}");
+}
