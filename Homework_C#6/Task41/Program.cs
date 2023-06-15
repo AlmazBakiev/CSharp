62e849280ff7ed90257d395636186c5a22441564
Almaz <bakievalmaz0@gmail.com>
2023-06-15 20:40:10 +0300

    fix

--- Homework_C#6/Task41/Program.cs
+++ Homework_C#6/Task41/Program.cs
@@ -7,10 +7,10 @@ int CountingPositiveNumbers()
     Console.WriteLine("Чтобы остановить ввод введте 'stop'!");
     while (true)
     {
-        if (strNumber.Equals("stop")) return counter;
-
         Console.Write("Введите число: ");
         string strNumber = Console.ReadLine()!;
+
+        if (strNumber.Equals("stop")) return counter;
 
         if (int.TryParse(strNumber, out int i))
         {
