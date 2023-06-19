db7e7c73702a067121542804ee265359f8373686
Almaz <bakievalmaz0@gmail.com>
2023-06-19 23:28:00 +0300

    homework9 completed

---
+++ Homework_C#9/Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int number = Prompt("Введите число: ");
+Console.WriteLine(PrintNumbers(number));
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+string PrintNumbers(int number)
+{
+    if (number == 1) return "1";
+
+    return $"{number} {PrintNumbers(number - 1)}";
+}
---
+++ Homework_C#9/Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int m = Prompt("Введите число m: ");
+int n = Prompt("Введите число n: ");
+Console.WriteLine(A(m, n));
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int A(int firstNumber, int secondNumber)
+{
+    if (firstNumber == 0) return secondNumber + 1;
+    else if (firstNumber != 0 && secondNumber == 0) return A(firstNumber - 1, 1);
+    else return A(firstNumber - 1, A(firstNumber, secondNumber - 1));
+}
